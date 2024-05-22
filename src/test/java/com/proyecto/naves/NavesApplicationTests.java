276407c89869681db93100d7a172d8bf750c5c96
HoroHoro79 <imanolperezpujante1@gmail.com>
2024-05-22 17:44:09 +0200

    Commit Inicial

---
+++ src/test/java/com/proyecto/naves/NavesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.naves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
